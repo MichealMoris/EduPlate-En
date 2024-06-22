27a498e4b39332f9b719d0d79555e6841346c334
MichealMoris <michealmoriswaheeb@gmail.com>
2024-06-22 22:27:12 +0300

    build added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import TeacherSlider from './TeachersSlider';
 import heroImg from './assets/hero_img.jpg';
 function App() {
   const phoneNumber = "1155225015";
-  const message = "Hello Mr Mohamed Adel, This is a demo message...";
+  const message = "Hello Mr Mohamed Adel, This is a demo message....";
   const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
   return (
     <div className="container-fluid">

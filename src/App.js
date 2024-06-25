00188dee3c2366c7d418c80dd5ae7be9c83f9124
MichealMoris <michealmoriswaheeb@gmail.com>
2024-06-25 21:47:36 +0300

    Navbar added...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import TeacherSlider from './TeachersSlider';
 import heroImg from './assets/hero_img.jpg';
+import logo from './assets/app_logo.png';
 function App() {
   const phoneNumber = "1155225015";
   const message = "Hello Mr Mohamed Adel, This is a demo message....";
   const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
   return (
     <div className="container-fluid">
+      <nav className="navbar navbar-expand-lg pt-3">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img src={logo} className='img-fluid' style={{width: 8+"rem"}}/>
+          </a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link">Contact us</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">About us</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Our Teachers</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <hr />
       <div className="row align-items-center mt-4">
         <div className="d-xxl-none d-xl-none d-lg-none d-md-none col-sm-12 d-sm-inline">
           <img src={heroImg} className='img-fluid' />
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import "./css/bootstrap/bootstrap.min.css";
 import "./css/style.css";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
